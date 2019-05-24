03f2a42f5ad3e06f0c6c30857ff9cb29bf0e8d8c
hamed4000 <hamed4000@yahoo.com>
2019-05-24 20:01:03 +0430

    Navbar component & create context for it

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
+import {ProductProvider} from "./context/Context";
 
-ReactDOM.render(<BrowserRouter>
-  <App />
-</BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+    <ProductProvider>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </ProductProvider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
