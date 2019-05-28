0293249742e0d2ba0f6213fec63200abd11998c1
hamed4000 <hamed4000@yahoo.com>
2019-05-28 19:36:30 +0430

    defaullt page

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import defaultBcg from "../images/defaultBcg.jpeg";
 
 const DefaultPage = () => {
     return (
         <>
-            <h1>default page</h1>
+          <Hero img={defaultBcg} max="true" title="404">
+            <h2 className="text-uppercase">page not found</h2>
+            <Link to="/" className="main-link" >
+              return home
+            </Link>
+          </Hero>
         </>
     );
 };
