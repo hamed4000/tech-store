b0cb0bcf41bdc440201c292320bbe3157fe89bf0
hamed4000 <hamed4000@yahoo.com>
2019-06-15 20:38:10 +0430

    set .env

--- src/context/ContentFullApi.js
+++ src/context/ContentFullApi.js
@@ -2,9 +2,9 @@ import * as contentFull from "contentful"
 
 const client = contentFull.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: "abic1vbbal4u",
+  space: process.env.REACT_APP_SPACE_ID,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "eN59GupN8ABxMF6yvS0nJ_v3AhoDyONyRdzGT55C3P4"
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
 
 export default client;
